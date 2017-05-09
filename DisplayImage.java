889efc20484578a9d32d8e4b4d324d4074bb3d33
SAFenton <SAFenton@student.naperville203.org>
2017-05-09 15:08:51 -0500

    My lovely shit moved around

--- DisplayImage.java
+++ DisplayImage.java
@@ -1,17 +1,21 @@
+ 
+
 import java.awt.*;  
 import java.applet.*;  
   
   
 public class DisplayImage extends Applet {  
   
-  Image picture;  
+  Image picture, pngpic;  
   
   public void init() {  
     picture = getImage(getDocumentBase(),"fuckme.jpg");  
+    pngpic = getImage(getDocumentBase(),"thisshit.png");  
   }  
     
   public void paint(Graphics g) {  
     g.drawImage(picture, 0,0, this);
+    g.drawImage(pngpic, 0, 0, this);
     
   }  
       
