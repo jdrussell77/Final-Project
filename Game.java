e9f994ee1f30c08e21ba59670d4e1da075deabde
sjpiazza99 <SJPiazza@student.naperville203.org>
2017-05-11 15:09:10 -0500

    Sam's 3rd commit
    Added methods HorizontalCheck, VerticalCheck, DiagonalsCheck to finish
    isGameOver method (which determines the end of the game). Also renamed
    whosNext method to whosUp.  Also added methods findXcord and findYcord
    to finish the move method. These methods find the x and y coordinate of
    the circle object created in move() which is dependent on row and column

--- Game.java
+++ Game.java
@@ -6,6 +6,7 @@ public class Game
 {
     private Player p1;
     private Player p2;
+    private final int radius=20;
     private boolean a=true;
     private boolean b=true;
     private Circle [][] board=new Circle[6][7];
@@ -16,18 +17,21 @@ public class Game
     {
         p1=a;//Sets player 1
         p2=b;//Sets player 2
-        
+
         p1.setNum(1);
         p2.setNum(2);
     }
+
     public Player getPlayer1()
     {
         return p1;
     }
+
     public Player getPlayer2()
     {
         return p2;
     }
+
     public boolean isFull()
     {
         for(int r=0;r<board.length;r++)
@@ -40,6 +44,52 @@ public class Game
         }
         return true;
     }
+
+    public boolean HorizontalCheck()
+    {
+        for(int r=0;r<board.length;r++)
+        {
+            for(int c=0;c<board[r].length-3;c++)
+            {
+                if(board[r][c].getColor()==board[r][c+1].getColor() && board[r][c].getColor()==board[r][c+2].getColor() && board[r][c].getColor()==board[r][c+3].getColor())        
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean VerticalCheck()
+    {
+        for(int r=0;r<board.length-3;r++)
+        {
+            for(int c=0;c<board[r].length;c++)
+            {          
+                if(board[r][c].getColor()==board[r+1][c].getColor() && board[r][c].getColor()==board[r+2][c].getColor() && board[r][c].getColor()==board[r+3][c].getColor())        
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean DiagonalsCheck()
+    {
+        // ascendingDiagonalCheck 
+        for (int r=3; r<board.length; r++){
+            for (int c=0; c<board[r].length-3; c++){
+                if (this.board[r][c] == this.board[r-1][c+1] && this.board[r][c] == this.board[r-2][c+2] && this.board[r][c] == this.board[r-3][c+3])
+                    return true;
+            }
+        }
+        // descendingDiagonalCheck
+        for (int r=3; r<board.length; r++){
+            for (int c=3; c<board[r].length; c++){
+                if (this.board[r][c] == this.board[r-1][c-1] && this.board[r][c] == this.board[r-2][c-2] && this.board[r][c] == this.board[r-3][c-3])
+                    return true;
+            }
+        }
+        return false;
+    }
+
     /*
      * Checks the state of the game (if someone has won and who)
      * should be run after each round
@@ -49,48 +99,21 @@ public class Game
         //Checking if board is full
         if(this.isFull())
             return true;
-            
+
         //Checking Horizontals
-        for(int r=0;r<board.length;r++)
-        {
-            for(int c=0;c<board[r].length-3;c++)
-            {
-                if(board[r][c].getColor()==board[r][c+1].getColor() && board[r][c].getColor()==board[r][c+2].getColor() && board[r][c].getColor()==board[r][c+3].getColor())        
-                    return true;
-            }
-        }
-        
+        if(this.HorizontalCheck())
+            return true;
+
         //Checking Columns
-        for(int r=0;r<board.length-3;r++)
-        {
-            for(int c=0;c<board[r].length;c++)
-            {          
-                if(board[r][c].getColor()==board[r+1][c].getColor() && board[r][c].getColor()==board[r+2][c].getColor() && board[r][c].getColor()==board[r+3][c].getColor())        
-                    return true;
-            }
-        }
+        if(this.VerticalCheck())
+            return true;
         
-        //Checking Left to Right Diagonal
-        for(int r=0;r<board.length;r++)
-        {
-            for(int c=board[r].length-1;c>=0;c--)
-            {          
-                if(board[r][c].getColor()==board[r+1][c].getColor() && board[r][c].getColor()==board[r+2][c].getColor() && board[r][c].getColor()==board[r+3][c].getColor())        
-                    return true;
-            }
-        }
-        
-        //Checking Right to Left Diagonal
-        for(int r=0;r<board.length;r++)
-        {
-            for(int c=board[r].length-1;c>=0;c--)
-            {          
-                if(board[r][c].getColor()==board[r+1][c].getColor() && board[r][c].getColor()==board[r+2][c].getColor() && board[r][c].getColor()==board[r+3][c].getColor())        
-                    return true;
-            }
-        }
+        if(this.DiagonalsCheck())
+            return true;
+            
         return false;
     }
+
     public void next()
     {
         if (a)
@@ -104,21 +127,36 @@ public class Game
             b=false;
         }
     }
-    public Player whosNext()
+
+    public Player whosUp()
     {
         if(a)
             return p1;
         else
             return p2;
     }
-    public void move(int col/*some mouse listener event to get which column in array*/)
+
+    public void move(int col)
     {
         if(this.isColumnFull(col)==false)// move is valid if the top column isn't full
         {
             int openRow=this.findOpenRow(col);//Finds the lowest point in column
-            board[openRow][col]=new Circle(10,10,10,this.whosNext().getColor());
+            board[openRow][col]=new Circle(findXcord(col),findYcord(openRow),radius,this.whosUp().getColor());
         }
     }
+
+    public int findXcord(int col)
+    {
+        int x=321+(int)(42.857*col);
+        return x;
+    }
+
+    public int findYcord(int row)
+    {
+        int y=229+(int)(58.333*row);
+        return y;
+    }
+
     public int findOpenRow(int col)
     {
         int r=0;
@@ -129,12 +167,14 @@ public class Game
         }
         return r-1;
     }
+
     public boolean isColumnFull(int col)
     {
         if(board[0][col].getColor()==null)
             return false;
         return true;
     }
+
     public Circle[][] getBoard()
     {
         return board;
