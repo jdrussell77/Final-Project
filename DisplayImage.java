bb378a03cd24bb582d98d51f57566c1876e8aa0e
SAFenton <sashafenton1999@msn.com>
2017-05-15 11:01:06 -0500

    grid resized

--- DisplayImage.java
+++ DisplayImage.java
@@ -3,18 +3,28 @@ import java.awt.*;
 import java.applet.*;  
 
 public class DisplayImage extends Applet {  
+    //graphisc
     private Image bgimage, leftllama, rightalpaca, asteroid1, asteroid2, gameboardborder, header,
     currentplayergraphic, player1icon, player2icon, restartbutton, menubutton, appletborder;
+    
+    //players
     private Player p1 = new Player(Color.red);
     private Player p2 = new Player(Color.yellow);
+    
+    //game
     private Game game=new Game(p1, p2);
-    private Grid grid=new Grid(350,200,200,350,Color.white);
+    
+    //gameboard
+    private Grid grid=new Grid(300, (700/3),300,350,Color.white);
     private Circle [][] board=new Circle[6][7];
+    
+    
+    
     public void init() {  
         //sets applet size
         setSize (900, 600);
 
-        //images that will be used
+        //assigns images that will be used to variables
         appletborder = getImage(getDocumentBase(), "appletborder.png");
         bgimage = getImage(getDocumentBase(),"spacebackground.png"); 
         leftllama = getImage(getDocumentBase(), "leftllama.png");
@@ -26,8 +36,8 @@ public class DisplayImage extends Applet {
         currentplayergraphic = getImage(getDocumentBase(), "currentplayer.png");
         player1icon = getImage(getDocumentBase(), "player1icon.png");
         player2icon = getImage(getDocumentBase(), "player2icon.png");
-        restartbutton = getImage(getDocumentBase(), "restartbutton.png");
-        menubutton = getImage(getDocumentBase(), "menubutton.png");
+        //restartbutton = getImage(getDocumentBase(), "restartbutton.png");
+        //menubutton = getImage(getDocumentBase(), "menubutton.png");
 
     }  
     public void paint(Graphics g) 
@@ -42,14 +52,20 @@ public class DisplayImage extends Applet {
         g.drawImage(header, (800/3), (100/3), this);
         g.drawImage(gameboardborder, 275, 175, this);
         g.drawImage(currentplayergraphic, (1000/3), (400/3), this);
-        g.drawImage(restartbutton, 100, (1600/3), this);
-        g.drawImage(menubutton, 700, (1600/3), this);
+        //g.drawImage(restartbutton, 100, (1600/3), this);
+        //g.drawImage(menubutton, 700, (1600/3), this);
+        
+        //draws the grid
+        
+        //draws the current player graphics
         grid.draw(g);
         if (game.whosUp().getColor()==p1.getColor())
             g.drawImage( player1icon, 0, 0, this);
         else 
             g.drawImage( player2icon, 0, 0, this);
 
+        
+            
         game.move(0);
         game.next();
         game.move(0);
